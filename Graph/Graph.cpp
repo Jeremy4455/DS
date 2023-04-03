d50f16323dbf43685eb14c3bb731f920f0cbbbc1
Jeremy_65536 <2696048261@qq.com>
2023-04-04 02:17:29 +0800

    Graph

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -3,23 +3,25 @@
 using namespace std;
 
 int main() {
-    Graph<string> g(5);
-    g.addVertex("A");
-    g.addVertex("B");
-    g.addVertex("C");
-    g.addVertex("D");
-    g.addVertex("E");
-    g.addEdge("A", "B");
-    g.addEdge("B", "C");
-    g.addEdge("C", "D");
-    g.addEdge("D", "E");
-    g.addEdge("A", "E");
+    vector<string> v = { "A", "B", "C", "D", "E", "F" };
+    Graph<string, int> g(6, v);
+    g.addEdge("A", "B", 4);
+    g.addEdge("A", "F", 2);
+    g.addEdge("B", "C", 6);
+    g.addEdge("B", "F", 5);
+    g.addEdge("C", "D", 3);
+    g.addEdge("C", "E", 1);
+    g.addEdge("C", "F", 3);
+    g.addEdge("D", "E", 7);
+    g.addEdge("E", "F", 8);
     cout << g;
-    cout << g.hasPath("A", "C") << endl; // true
-    cout << g.hasPath("A", "D") << endl; // true
-    cout << g.hasPath("A", "F") << endl; // false
+    cout << g.hasPath("A", "C") << endl;
+    cout << g.hasPath("A", "D") << endl;
+    cout << g.hasPath("A", "F") << endl;
     cout << "All paths between vertices A and D:" << endl;
     g.printAllPaths("A", "D");
+    cout << g.Kruskal();
+    cout << g.Prim();
     return 0;
 }
 
--- Graph/Graph.h
+++ Graph/Graph.h
@@ -2,36 +2,42 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-template <typename T>
+template <typename T, typename W>
 class Graph {
 private:
 	vector<T> vertex;        // 存放顶点信息的数组
-	vector<vector<int> > adj; // 存放边及其权重的邻接矩阵
+	vector<vector<W>> adj;	 // 存放边及其权重的邻接矩阵
 	int numVertices;         // 顶点数目
 	int maxVertices;         // 允许的顶点最大数目
 	int numEdges;            // 边数
 	int findIndex(const T& v);  //根据顶点名称找到索引
 	bool hasPathHelper(int i, int j, vector<bool>& visited);    //辅助判断两个顶点是否有路径
 	void printAllPathsHelper(int i, int j, vector<bool>& visited, vector<int>& path);   //辅助找出两个顶点间的所有路径
-
+	int findParent(vector<int>& parent, int u);
 public:
 	Graph(int maxV);										//	构造函数
+	Graph(int maxV, vector<T>vertex);										//	构造函数
 	~Graph();												//	析构函数
-	Graph(const Graph<T>& other);							//	拷贝构造函数
+	Graph(const Graph<T, W>& other);							//	拷贝构造函数
 	void addVertex(T v);									//	添加顶点
 	void addEdge(const T& v1, const T& v2, int weight=1);	//	添加边
+	void addEdge(int i, int j, int weight);
 	bool hasPath(const T& v1, const T& v2);					//	判断两个顶点间是否存在路径
 	void printAllPaths(const T& v1, const T& v2);			//	输出顶点间所有简单路径
 	void Display() const;									//	输出函数
-	Graph<T>& operator=(const Graph<T>& other);				//	赋值运算符重载
-	//friend ostream& operator<<(ostream& os, const Graph<T>& graph);	由于定义了Display函数，所以不需要定义为友元函数即可实现
+	Graph<T, W>& operator=(const Graph<T, W>& other);				//	赋值运算符重载
+	Graph<T, W> Kruskal();										//	Kruskal算法
+	Graph<T, W> Prim();										//	Prim算法
+	//friend ostream& operator<<(ostream& os, const Graph<T, W>& graph);	由于定义了Display函数，所以不需要定义为友元函数即可实现
+
 };
 
-template<typename T>
-inline Graph<T>::Graph(int maxV) {
+template <typename T, typename W>
+inline Graph<T, W>::Graph(int maxV) {
 	numVertices = 0;
 	maxVertices = maxV;
 	numEdges = 0;
@@ -41,8 +47,24 @@ inline Graph<T>::Graph(int maxV) {
 	}
 }
 
-template<typename T>
-inline Graph<T>::~Graph()
+template <typename T, typename W>
+inline Graph<T, W>::Graph(int maxV, vector<T> vertex)
+{
+	numVertices = vertex.size();
+	maxVertices = maxV;
+	numEdges = 0;
+	adj.resize(maxV);
+	for (int i = 0; i < maxV; i++) {
+		adj[i].resize(maxV);
+	}
+	this->vertex.resize(numVertices);
+	for (int i = 0; i < numVertices; i++) {
+		this->vertex[i] = vertex[i];
+	}
+}
+
+template <typename T, typename W>
+inline Graph<T, W>::~Graph()
 {
 	for (int i = 0; i < maxVertices; i++) {
 		adj[i].clear();
@@ -51,14 +73,19 @@ inline Graph<T>::~Graph()
 	vertex.clear();
 }
 
-template<typename T>
-inline Graph<T>::Graph(const Graph<T>& other)
+template <typename T, typename W>
+inline Graph<T, W>::Graph(const Graph<T, W>& other)
 {
 	this->maxVertices = other.maxVertices;
 	this->numVertices = other.numVertices;
 	this->numEdges = other.numEdges;
+	this->vertex.resize(numVertices);
 	for (int i = 0; i < other.numVertices; i++) {
 		this->vertex[i] = other.vertex[i];
+	}
+	this->adj.resize(maxVertices);
+	for (int i = 0; i < maxVertices; i++) {
+		adj[i].resize(maxVertices);
 	}
 	for (int i = 0; i < other.numVertices; i++) {
 		for (int j = 0; j < other.numVertices; j++) {
@@ -67,8 +94,8 @@ inline Graph<T>::Graph(const Graph<T>& other)
 	}
 }
 
-template<typename T>
-inline void Graph<T>::addVertex(T v) {
+template <typename T, typename W>
+inline void Graph<T, W>::addVertex(T v) {
 	if (numVertices < maxVertices) {
 		vertex.push_back(v);
 		numVertices++;
@@ -78,8 +105,8 @@ inline void Graph<T>::addVertex(T v) {
 	}
 }
 
-template<typename T>
-inline void Graph<T>::addEdge(const T& v1, const T& v2, int weight) {
+template <typename T, typename W>
+inline void Graph<T, W>::addEdge(const T& v1, const T& v2, int weight) {
 	int i = findIndex(v1);
 	int j = findIndex(v2);
 	if (i >= 0 && j >= 0) {
@@ -92,8 +119,20 @@ inline void Graph<T>::addEdge(const T& v1, const T& v2, int weight) {
 	}
 }
 
-template<typename T>
-inline int Graph<T>::findIndex(const T& v) {
+template <typename T, typename W>
+inline void Graph<T, W>::addEdge(int i, int j, int weight) {
+	if (i >= 0 && j >= 0) {
+		adj[i][j] = weight;
+		adj[j][i] = weight;
+		numEdges++;
+	}
+	else {
+		cout << "Invalid vertices" << endl;
+	}
+}
+
+template <typename T, typename W>
+inline int Graph<T, W>::findIndex(const T& v) {
 	for (int i = 0; i < numVertices; i++) {
 		if (vertex[i] == v) {
 			return i;
@@ -102,8 +141,8 @@ inline int Graph<T>::findIndex(const T& v) {
 	return -1;
 }
 
-template<typename T>
-inline bool Graph<T>::hasPath(const T& v1, const T& v2) {
+template <typename T, typename W>
+inline bool Graph<T, W>::hasPath(const T& v1, const T& v2) {
 	int i = findIndex(v1);
 	int j = findIndex(v2);
 	if (i < 0 || j < 0) {
@@ -113,8 +152,8 @@ inline bool Graph<T>::hasPath(const T& v1, const T& v2) {
 	return hasPathHelper(i, j, visited);
 }
 
-template<typename T>
-inline bool Graph<T>::hasPathHelper(int i, int j, vector<bool>& visited) {
+template <typename T, typename W>
+inline bool Graph<T, W>::hasPathHelper(int i, int j, vector<bool>& visited) {
 	visited[i] = true;
 	if (i == j) {
 		return true;
@@ -129,8 +168,8 @@ inline bool Graph<T>::hasPathHelper(int i, int j, vector<bool>& visited) {
 	return false;
 }
 
-template<typename T>
-inline void Graph<T>::printAllPaths(const T& v1, const T& v2) {
+template <typename T, typename W>
+inline void Graph<T, W>::printAllPaths(const T& v1, const T& v2) {
 	int i = findIndex(v1);
 	int j = findIndex(v2);
 	if (i < 0 || j < 0) {
@@ -139,12 +178,11 @@ inline void Graph<T>::printAllPaths(const T& v1, const T& v2) {
 	}
 	vector<bool> visited(numVertices, false);
 	vector<int> path;
-	path.push_back(i);
 	printAllPathsHelper(i, j, visited, path);
 }
 
-template<typename T>
-inline void Graph<T>::printAllPathsHelper(int i, int j, vector<bool>& visited, vector<int>& path) {
+template <typename T, typename W>
+inline void Graph<T, W>::printAllPathsHelper(int i, int j, vector<bool>& visited, vector<int>& path) {
 	visited[i] = true;
 	path.push_back(i);
 	if (i == j) {
@@ -164,8 +202,20 @@ inline void Graph<T>::printAllPathsHelper(int i, int j, vector<bool>& visited, v
 	path.pop_back();
 }
 
-template<typename T>
-inline void Graph<T>::Display() const{
+//辅助找出两个顶点间的所有路径
+template <typename T, typename W>
+inline int Graph<T, W>::findParent(vector<int>& parent, int u)
+{
+	if (parent[u] == u)
+	{
+		return u;
+	}
+
+	return findParent(parent, parent[u]);
+}
+
+template <typename T, typename W>
+inline void Graph<T, W>::Display() const{
 	cout << "Vertices: ";
 	for (int i = 0; i < numVertices; i++) {
 		cout << vertex[i] << " ";
@@ -181,26 +231,109 @@ inline void Graph<T>::Display() const{
 	}
 }
 
-template <typename T>
-Graph<T>& Graph<T>::operator=(const Graph<T>& other) {
+template <typename T, typename W>
+Graph<T, W>& Graph<T, W>::operator=(const Graph<T, W>& other) {
 	if (this != &other) {
 		this->maxVertices = other.maxVertices;
 		this->numVertices = other.numVertices;
 		this->numEdges = other.numEdges;
-		this->vertex.clear();
+		this->vertex.resize(numVertices);
 		for (int i = 0; i < other.numVertices; i++) {
-			this->vertex.push_back(other.vertex[i]);
+			this->vertex[i] = other.vertex[i];
 		}
-		this->adj.clear();
+		this->adj.resize(maxVertices);
+		for (int i = 0; i < maxVertices; i++) {
+			adj[i].resize(maxVertices);
+		}
 		for (int i = 0; i < other.numVertices; i++) {
-			this->adj.push_back(other.adj[i]);
+			for (int j = 0; j < other.numVertices; j++) {
+				this->adj[i][j] = other.adj[i][j];
+			}
 		}
 	}
 	return *this;
 }
 
-template <typename T>
-ostream& operator<<(ostream& os, const Graph<T>& graph) {
+template <typename T, typename W>
+inline Graph<T, W> Graph<T, W>::Kruskal()
+{
+	Graph<T, W> mst(maxVertices, vertex);
+	//初始化一个边的列表并将其按权值排序
+	vector<pair<int, pair<int, int>>> edges;
+	for (int i = 0; i < numVertices; i++)
+	{
+		for (int j = i + 1; j < numVertices; j++)
+		{
+			if (adj[i][j] != 0)
+			{
+				edges.push_back(make_pair(adj[i][j], make_pair(i, j)));
+			}
+		}
+	}
+	sort(edges.begin(), edges.end());
+	//定义一个parent数组来存储每个顶点的根节点，来实现并查集合并
+	vector<int> parent(numVertices);
+	for (int i = 0; i < numVertices; i++)
+	{
+		parent[i] = i;
+	}
+	for (int i = 0; i < edges.size(); i++)
+	{
+		int weight = edges[i].first;
+		int u = edges[i].second.first;
+		int v = edges[i].second.second;
+		int parentU = findParent(parent, u);
+		int parentV = findParent(parent, v);
+		if (parentU != parentV)
+		{
+			mst.addEdge(u, v, weight);
+			parent[parentU] = parentV;
+		}
+	}
+	return mst;
+}
+
+template <typename T, typename W>
+inline Graph<T, W> Graph<T, W>::Prim()
+{
+	Graph<T, W> mst(numVertices);
+	if (numVertices == 0)	return mst;
+	vector<bool> visited(numVertices, false);
+	visited[0] = true;  
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	mst.addVertex(vertex[0]);
+	for (int j = 0; j < numVertices; j++) {
+		if (adj[0][j] != 0) {
+			pq.push({ adj[0][j], j });
+		}
+	}
+	int from = 0;
+	for (int i = 1; i < numVertices; i++) {
+		while (!pq.empty() && visited[pq.top().second]) {
+			pq.pop();
+		}
+		if (pq.empty()) {
+			break;
+		}
+		int src = pq.top().second;
+		int weight = pq.top().first;
+		pq.pop();
+		visited[src] = true;
+		mst.addVertex(vertex[src]);
+		mst.addEdge(vertex[from], vertex[src], weight);
+		from = src;
+		for (int j = 0; j < numVertices; j++) {
+			if (adj[src][j] != 0 && !visited[j]) {
+				pq.push({ adj[src][j], j });
+			}
+		}
+	}
+	return mst;
+}
+
+
+template <typename T, typename W>
+ostream& operator<<(ostream& os, const Graph<T, W>& graph) {
 	graph.Display();
 	return os;
 }
